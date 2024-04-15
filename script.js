3ca61662980cfca83fb84554209460c512df5ea6
HaseebullahMiakhil <tw773029@gmail.com>
2024-04-15 09:37:26 +0430

    spread operator

--- script.js
+++ script.js
@@ -17,9 +17,16 @@ const getMode = (array) => {
   if (new Set(Object.values(counts)).size === 1) {
     return null;
   }
-  const highest = Object.keys(counts);
+  const highest = Object.keys(counts).sort(
+    (a, b) => counts[b] - counts[a]
+  )[0];
+  const mode = Object.keys(counts).filter(
+    (el) => counts[el] === counts[highest]
+  );
+  return mode.join(", ");
 }
 
+const getRange = (array) =>{}
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
@@ -28,7 +35,9 @@ const calculate = () => {
   
   const mean = getMean(numbers);
   const median = getMedian(numbers);
+  const mode = getMode(numbers);
 
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
+  document.querySelector("#mode").textContent = mode;
 }
